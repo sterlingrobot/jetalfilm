dc65a0b587cc6ac954565ed155cece30dce3c7a6
Tor Gilbertson <sterlingrobot@gmail.com>
2015-05-31 21:58:40 -0600

    Add bootstrap, site css

--- index.php
+++ index.php
@@ -1,11 +1,62 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-	<title>J et.al: A documentary film exploring Disassociative Identity Disorder</title>
+	<title>J et al. :: A documentary exploring Disassociative Identity Disorder</title>
+
+	<!-- General META -->
+	<meta charset="utf-8">
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+
+	<!-- Semantic META -->
+	<meta name="keywords" content="{{keywords}}">
+	<meta name="description" content="{{description}}">
+
+	<!-- Facebook META -->
+	<meta property="fb:app_id" content="{{facebookAppId}}">
+	<meta property="og:site_name" content="{{title}}">
+	<meta property="og:title" content="{{title}}">
+	<meta property="og:description" content="{{description}}">
+	<meta property="og:url" content="{{url}}">
+	<meta property="og:image" content="/img/brand/logo.png">
+	<meta property="og:type" content="website">
+
+	<!-- Twitter META -->
+	<meta name="twitter:title" content="{{title}}">
+	<meta name="twitter:description" content="{{description}}">
+	<meta name="twitter:url" content="{{url}}">
+	<meta name="twitter:image" content="/img/brand/logo.png">
+
+	<!-- Fav Icon -->
+	<link href="/modules/core/img/brand/favicon.ico" rel="shortcut icon" type="image/x-icon">
+
+
+	<!-- build:css css/jetal.min.css -->
+	<link href="vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="css/style.css" rel="stylesheet" media="screen">
+	<!-- endbuild -->
+
+	<!-- HTML5 Shim -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
 </head>
 <body>
-	<h1>J et.al.<br>
-		<small>One Man's Journey to Become One Man</small>
-	</h1>
+	<div class="container">
+
+		<h1>J et al.<br>
+			<small>One Man's Journey<br>To Become One Man</small>
+		</h1>
+
+		<p>Jason's mind first split to protect him from ongoing trauma when he was six. He's now 39 with 26 known alters. "Ethan" (8) lives in a castle with knights, trap doors, and big battles. "Peter" (6) worries that he has Alzheimers, but none of the "adults" listen to his concerns. And when "The Bullet" has control, Jason wakes up in the hospital after attempting suicide.</p>
+
+		<p>But Jason doesn't let life f*ck him. After living on the streets of Chicago, "The Boyfriend" followed a woman to LA. Now he's living in "Purgatory House" - a halfway house he desperately wants to move out of. With help from Dr. Paula, Jason is struggling to put himself back together so he can have a safe home for the first time in his life.</p>
+	</div>
+	<!-- build:js js/jetal.min.js -->
+
+	<!-- endbuild -->
 </body>
 </html>
